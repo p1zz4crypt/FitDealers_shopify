0b836eac4765c14dcdc0c2b5b6cae0458d447ac7
p1zz4crypt <p1zz4crypt@gmail.com>
2025-07-09 17:32:58 -0600

    feat: Agregar sección de marquesina de marcas y bloque de testimonio; actualizar configuraciones de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
       }
     },
     "extend": {
+       animation: {
+        marquee: 'marquee 30s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      },
       "colors": {
         "primary": "#FFC800",
         "black": "#000000",
